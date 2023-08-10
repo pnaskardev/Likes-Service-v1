fd0aca62d7e767cbe3c61cdfe1ec8ab43028f989
pnaskardev <priyanshunaskar89@gmail.com>
2023-08-10 22:02:54 +0530

    added created_by tag in Post

--- Post/models.py
+++ Post/models.py
@@ -2,12 +2,12 @@ from django.contrib.auth.models import AbstractBaseUser
 
 from django.db import models
 
-
-class User(models.Model):
-    pass
-
+from User.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     image = models.CharField(max_length=200, blank=True)
     likes_count = models.IntegerField(default=0)
+
+    created_by = models.ForeignKey(
+        User, related_name='posts', on_delete=models.CASCADE)

6d4312b98aaa9f62cf796ea21fac1e77f2b7e975
pnaskardev <priyanshunaskar89@gmail.com>
2023-08-11 17:15:07 +0530

    made some optimising changes

--- User/models.py
+++ User/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
-from django.utils import timezone
 
 
 class CustomUserManager(BaseUserManager):
